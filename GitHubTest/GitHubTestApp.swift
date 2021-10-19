b572152d07e540b463ae98b3562fe32e6581a67b
5boltof <5boltof@royalhospitalschool.org>
2021-10-19 12:51:17 +0100

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Freddie Bolton on 19/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
